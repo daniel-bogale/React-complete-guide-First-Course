575ce73102ecd2d170280abf906394b6c0c9e436
danibog <dannybg090909@gmail.com>
2023-04-18 23:38:44 -0700

    📁expenseItem is modified

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
 
-/* ReactDOM.render(<App />, document.getElementById("root")); */
+ 
